ee22f8338bc1a877cbf4c4f6126602047303ef91
vincentgu818 <vincentgu818@gmail.com>
2019-02-11 12:29:37 -0500

    added seed route

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users.js");
+const userSeed = require("../models/users_seed.js")
 const bcrypt = require("bcrypt");
 
+// create route
 router.post("/", (req, res) => {
    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
    User.create(req.body, (err, createdUser) => {
@@ -13,18 +15,31 @@ router.post("/", (req, res) => {
    });
 });
 
+// seed route
+router.get("/seed", (req, res) => {
+   User.create(userSeed, (err, createdUsers) => {
+     res.status(201).json({
+        status: 201,
+        message: "user database seeded"
+      });
+   });
+});
+
+// read route
 router.get("/", (req, res) => {
    User.find({}, (err, foundUsers) => {
       res.json(foundUsers);
    });
 });
 
+// delete route
 router.delete("/:id", (req, res) => {
    User.findByIdAndRemove(req.params.id, (err, removedUser) => {
       res.json(removedUser);
    });
 });
 
+// update route
 router.put("/:id", (req, res) => {
    User.findByIdAndUpdate(
       req.params.id,
